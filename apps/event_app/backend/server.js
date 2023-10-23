144410a45b35379ebfcbac13e6d2e894d8232665
Chase Van Halen <cvanhalen@perscholas.org>
2023-10-23 10:46:50 -0700

    evetns

--- apps/event_app/backend/server.js
+++ apps/event_app/backend/server.js
@@ -21,6 +21,14 @@ app.use(helmet());
 
 // START ROUTES //
 
+// get the events
+
+
+app.get("/events", async (req, res) => {
+    let arrayOfEvents = await Event.find();
+    res.send(arrayOfEvents);
+})
+
 
 app.post("/events", async (req, res) => {
     // 1. get the data that was sent from the frontend
